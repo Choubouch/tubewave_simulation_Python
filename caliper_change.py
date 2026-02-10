ce4f44a59e00e507e94c5b8f775751e0f737e38b
Mathis <mathis.verdan@etu.minesparis.psl.eu>
2026-02-10 16:08:09 +0100

    copie grosse boucle for jusqu'à ligne 213 du code github

--- caliper_change.py
+++ caliper_change.py
@@ -37,5 +37,52 @@ delay = 1/(f0**2)
 
 Ricker = 2*PI**2*f0**2*(1.0 - 2.0*(PI**2)*(f0**2)*((tvec-delay)**2))*np.exp(-(PI**2)*(f0**2)*((tvec-delay)**2))
 Ricker = Ricker/np.max(np.abs(Ricker))
-
 fRicker = np.conj(np.fft.fft(Ricker))
+
+shift_z = 0
+zn_proc = zn_org + shift_z
+
+uEvec_allfreq = np.zeros((2, n, nw_proc), dtype=np.complex128)
+
+for iw in range(1, nw_proc):
+    w = wvec_proc[iw]
+    kp = w/Vpvec
+    
+    Mn = np.zeros((2,2,n-1), dtype=np.complex128)
+    for i_n in range(0, n-1):
+        kp1 = kp[i_n]
+        kp2 = kp[i_n+1]
+        z1 = zn_proc[i_n]
+        rho1 = Rhovec[i_n]
+        rho2 = Rhovec[i_n+1]
+        
+        a1 = rho2/rho1
+        a2 = kp2/kp1 
+        
+        m11 = 1.0/(2.0)*(a1+a2)*np.exp(1j*(kp1-kp2)*z1) 
+        m12 = 1.0/(2.0)*(a1-a2)*np.exp(1j*(kp1+kp2)*z1) 
+        m21 = 1.0/(2.0)*(a1-a2)*np.exp(-1j*(kp1+kp2)*z1) 
+        m22 = 1.0/(2.0)*(a1+a2)*np.exp(-1j*(kp1-kp2)*z1) 
+        
+        Mn[:,:,i_n] = np.array([[m11, m12], [m21, m22]], dtype=np.complex128)
+
+    MT = np.eye(2)
+    for i_n in range(n-1):
+        MT = np.matmul(MT,Mn[:, :, i_n])
+    
+    uEvec = np.zeros((2,n), dtype=np.complex128)
+
+    D1 = 1/(-Rhovec[1]*w**2)
+    
+    D1 = D1 * np.exp(1j*kp[1]*(-shift_z))
+
+    Dn = D1/MT[1, 1]
+    uEvec[:, n-1] = np.array([0, Dn])
+
+    for i_n in range(n-2, -1, -1):
+        uEvec[:,i_n] = Mn[:,:,i_n]@uEvec[:,i_n+1]    
+
+    U1=MT[0,1]/MT[1,1]*D1
+
+    uEvec_allfreq[:,:,iw]=uEvec
+    print(uEvec_allfreq[:,:,iw])
